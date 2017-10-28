5119c556e1a28fd42282389dece15f38afb4dbb8
Calvin Sienatra <31559240+calvinsienatra@users.noreply.github.com>
2017-10-28 17:30:22 -0500

    Configured app key and changed to debug mode

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'jFW3UuWlSPkB7X9TZkrTXDJww0jJbUvT'),
 
     'cipher' => 'AES-256-CBC',
 
